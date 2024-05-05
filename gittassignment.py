73389f597b456e08fe025fc6cb48f06774238fcc
Maman20 <Abdirahmanh166@gmail.com>
2024-05-05 11:35:36 -0500

    first commit

---
+++ gittassignment.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('gitassignment.html')  # Ensure your HTML file is named index.html or update this line accordingly.
+
+if __name__ == '__main__':
+    app.run(debug=True)
