a9c5bc602896cf2f1cb0ca7a4c7a2ecfa745be4e
Maman20 <Abdirahmanh166@gmail.com>
2024-05-05 12:08:02 -0500

    message

--- gittassignment.py
+++ gittassignment.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+#hello world
 @app.route('/')
 def home():
     return render_template('gitassignment.html')  # Ensure your HTML file is named index.html or update this line accordingly.
